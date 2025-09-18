ae3a485535ff4c38c05f71ca113d3dc59edc46ca
lalitm1004 <lalitm1004@gmail.com>
2025-09-18 19:06:18 +0530

    feat: add documentation

--- src/funny_numbers/__init__.py
+++ src/funny_numbers/__init__.py
@@ -2,4 +2,4 @@ from ._core import FunnyNumber
 from ._core import DeterministicFunnyNumberFactory, RandomFunnyNumberFactory
 
 __all__ = ["FunnyNumber", "DeterministicFunnyNumberFactory", "RandomFunnyNumberFactory"]
-__version__ = "0.1.0"
+__version__ = "0.2.0"
